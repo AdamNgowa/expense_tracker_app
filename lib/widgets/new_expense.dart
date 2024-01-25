6a0abfaf68cf63824e0eb9df35cd237142782da7
AdamNgowa <adamngowa3@gmail.com>
2024-01-25 22:15:34 +0300

    1.5.2

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -72,92 +72,96 @@ class _NewExpenseState extends State<NewExpense> {
         category: _selectedCategory,
       ),
     );
+    //Ensures that the overlay closes after every expense
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(15),
-      child: Column(
-        children: [
-          //Start of text field
-          TextField(
-            controller: _titleController,
-            maxLength: 50,
-            decoration: const InputDecoration(
-              label: Text('Enter the expense title'),
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  controller: _amountController,
-                  decoration: const InputDecoration(
-                    label: Text('Enter the amount'),
-                    prefixText: '\$',
-                  ),
-                  keyboardType: TextInputType.number,
-                ),
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 48, 15, 15),
+        child: Column(
+          children: [
+            //Start of text field
+            TextField(
+              controller: _titleController,
+              maxLength: 50,
+              decoration: const InputDecoration(
+                label: Text('Enter the expense title'),
               ),
-              //End of text field
-              const SizedBox(width: 15),
-              Expanded(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    //This code allows the selected date to automatically appear
-                    Text(
-                      _selectedDate == null
-                          ? 'No Selected Date'
-                          : formatter.format(_selectedDate!),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _amountController,
+                    decoration: const InputDecoration(
+                      label: Text('Enter the amount'),
+                      prefixText: '\$',
                     ),
-                    IconButton(
-                      onPressed: _presentDatePicker,
-                      icon: const Icon(Icons.calendar_month_sharp),
-                    )
-                  ],
+                    keyboardType: TextInputType.number,
+                  ),
                 ),
+                //End of text field
+                const SizedBox(width: 15),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      //This code allows the selected date to automatically appear
+                      Text(
+                        _selectedDate == null
+                            ? 'No Selected Date'
+                            : formatter.format(_selectedDate!),
+                      ),
+                      IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.calendar_month_sharp),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            //Save Expense and Cancel button
+            Row(children: [
+              //Dropdown button for category selection
+              DropdownButton(
+                value: _selectedCategory,
+                items: Category.values
+                    .map(
+                      (category) => DropdownMenuItem(
+                        value: category,
+                        child: Text(category.name.toUpperCase()),
+                      ),
+                    )
+                    .toList(),
+                onChanged: (value) {
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
+                },
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: _submitExpenseData,
+                child: const Text('Save Expense'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
               )
-            ],
-          ),
-          //Save Expense and Cancel button
-          Row(children: [
-            //Dropdown button for category selection
-            DropdownButton(
-              value: _selectedCategory,
-              items: Category.values
-                  .map(
-                    (category) => DropdownMenuItem(
-                      value: category,
-                      child: Text(category.name.toUpperCase()),
-                    ),
-                  )
-                  .toList(),
-              onChanged: (value) {
-                if (value == null) {
-                  return;
-                }
-                setState(() {
-                  _selectedCategory = value;
-                });
-              },
-            ),
-            const SizedBox(width: 10),
-            ElevatedButton(
-              onPressed: _submitExpenseData,
-              child: const Text('Save Expense'),
-            ),
-            const SizedBox(width: 10),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Cancel'),
-            )
-          ]),
-        ],
+            ]),
+          ],
+        ),
       ),
     );
   }
